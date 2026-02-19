e6b1d2b5d5485ff2d15651f4878da2151449b781
ccarreira <cesar.carreira@gmail.com>
2026-02-19 15:09:45 +0000

    ///

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,12 +2,20 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Programa de introduçao ao C#
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Imprime mensagens para consola
+        /// </summary>
+        /// <param name="args">Sem params</param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP!");
             Console.WriteLine("Bye world");
         }
+        
     }
 }
