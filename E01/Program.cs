517c96501044eeaeb7a5df107e66b70d8777cfc6
ccarreira <cesar.carreira@gmail.com>
2026-02-19 16:16:18 +0000

    code output

--- E01/Program.cs
+++ E01/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+//using System.Text;
 
 namespace E01
 {
     public class Program
     {
+        static string registered = "U+24C7";
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            //Console.OutputEncoding = Encoding.UTF8; 
+            Console.WriteLine(registered);
         }
     }
 }
